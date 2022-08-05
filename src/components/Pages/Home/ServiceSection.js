dda16f93143481b393224bbd26a508059e48d577
arfat132 <arfatbegum90@gmail.com>
2022-08-05 19:08:28 +0300

    service section updated

--- src/components/Pages/Home/ServiceSection.js
+++ src/components/Pages/Home/ServiceSection.js
@@ -1,39 +1,36 @@
 import React from 'react';
-import fluorida from '../../Assests/icons/fluoride.png';
-import cavity from '../../Assests/icons/fluoride.png';
-import teeth from '../../Assests/icons/whitening.png';
 const ServiceSection = () => {
     return (
-        <section className="text-gray-600 body-font">
+        <section className="text-gray-600 body-font px-12">
             <div className="container px-5 pb-24 mx-auto">
                 <div className="text-center mb-20">
                     <h3 className='text-secondary font-bold text-xl'>Our Services</h3>
-                    <h1 className="sm:text-3xl text-2xl title-font text-gray-900 mb-4">Services We Provide</h1>
+                    <h1 className="sm:text-3xl text-2xl title-font text-accent mb-4 font-bold">Take The Road To A Healthy Heart Beat</h1>
                 </div>
                 <div className="flex flex-wrap -m-4 text-center">
                     <div className="p-4 md:w-1/3">
-                        <div className="h-full shadow-lg border-2 hover:border-secondary border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                            <img className='mx-auto pt-8' src={fluorida} alt="" srcSet="" />
+                        <div className="h-full shadow-lg border-l-4 border-l-secondary rounded-none overflow-hidden">
+                            <img className='mx-auto w-40 pt-8' src="https://themetechmount.com/html/dezily/images/cardiology.svg" alt="" srcSet="" />
                             <div className="p-6">
-                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Fluoride Treatment</h2>
+                                <h2 className="text-accent text-2xl title-font font-bold mb-3">Heart Screening Technologies</h2>
+                                <p className="leading-relaxed text-base">The heart screening and detection is the essential to the manage and even preventing disease.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-4 md:w-1/3">
+                        <div className="h-full shadow-lg border-l-4 border-l-secondary rounded-none overflow-hidden">
+                            <img className='mx-auto w-40 pt-8' src="https://themetechmount.com/html/dezily/images/healthcare.svg" alt="" srcSet="" />
+                            <div className="p-6">
+                                <h2 className="text-gray-900 text-2xl title-font font-bold mb-3">Pacemaker Implant Techniquies</h2>
                                 <p className="leading-relaxed text-base">Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the.</p>
                             </div>
                         </div>
                     </div>
                     <div className="p-4 md:w-1/3">
-                        <div className="h-full shadow-lg border-2 hover:border-secondary border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                            <img className='mx-auto pt-8' src={cavity} alt="" srcSet="" />
+                        <div className="h-full shadow-lg border-l-4 border-l-secondary rounded-none overflow-hidden">
+                            <img className='mx-auto w-40 pt-8' src="https://themetechmount.com/html/dezily/images/heart.svg" alt="" srcSet="" />
                             <div className="p-6">
-                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Cavity Filling</h2>
-                                <p className="leading-relaxed text-base">Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="p-4 md:w-1/3">
-                        <div className="h-full shadow-lg border-2 hover:border-secondary border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                            <img className='mx-auto pt-8' src={teeth} alt="" srcSet="" />
-                            <div className="p-6">
-                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Teeth Whitening</h2>
+                                <h2 className="text-gray-900 text-2xl title-font mb-3 font-bold">Cardiovascular Care Services</h2>
                                 <p className="leading-relaxed text-base">Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the.</p>
                             </div>
                         </div>
