3c93dc4203deee2d24595bafb4add05fd25a8b5c
arfat132 <arfatbegum90@gmail.com>
2022-08-06 22:32:01 +0300

    departments section implemented

---
+++ src/components/Pages/Home/Departments.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+const Departments = () => {
+    return (
+        <section class="bg-gray-100 lg:px-12">
+            <div class="container px-5 py-24 mx-auto">
+            <div className="text-center mb-12">
+                    <h3 className='text-secondary font-bold text-xl mb-3'>Our Departments</h3>
+                    <h1 className="sm:text-3xl text-2xl title-font text-accent font-bold">Take The Road To A Healthy Lifestyle</h1>
+                </div>
+                <div class="flex flex-wrap -m-4 text-center">
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/0FZJdhW/dept1.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Cardiology</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/bvv4z97/dept2.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Rheumatology</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/wRzG9qG/dept3.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Neurology</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/VNKXmk7/dept4.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Podiatry</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/mSTys4G/dept5.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Radiation</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/f1xyFsy/dept6.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Dermatology</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/0JySmV6/dept7.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Gastroenterology</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/MBQChZn/dept8.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Pediatrician</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/tPMYpv3/dept9.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Otolaryngology</p>
+                        </div>
+                    </div>
+                    <div class="p-4 md:w-1/5 sm:w-1/2 w-full">
+                        <div class="bg-white border-l-4 border-l-secondary px-4 py-6 shadow-lg">
+                            <img className='mx-auto' src="https://i.ibb.co/hLxQrLz/dept10.png" alt="" />
+                            <p class="leading-relaxed font-bold mt-4 text-xl text-accent">Hematology</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Departments;
